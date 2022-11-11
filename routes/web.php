bc2615df9c6c07ff4137720dbbd05f975bac850e
Matthieu MARTIN <codingstuff91@gmail.com>
2022-11-11 16:51:12 +0100

    reset password routes

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@
 Route::get('login')->name('login')->uses('Auth\LoginController@showLoginForm')->middleware('guest');
 Route::post('login')->name('login.attempt')->uses('Auth\LoginController@login')->middleware('guest');
 Route::post('logout')->name('logout')->uses('Auth\LoginController@logout');
+Route::get('reset_password')->name('auth.forgot_password')->uses('Auth\ForgotPasswordController@forgotPasswordPage');
+Route::post('reset_password')->name('auth.request_password')->uses('Auth\ForgotPasswordController@requestNewPassword');
 
 // Dashboard
 Route::get('/')->name('dashboard')->uses('DashboardController')->middleware('auth');
